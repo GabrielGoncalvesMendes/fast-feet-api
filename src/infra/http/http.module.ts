1c3d5a9305aa1515ea2c9bbb51c2243506aa720d
Gabriel Mendes <gagmendes93@gmail.com>
2025-03-24 11:32:20 -0300

    Fix: Adding Repositories Layer

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -3,15 +3,15 @@ import { AuthenticateController } from './controllers/authenticate.controller'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { CreateQuestionsController } from './controllers/create-questions.controller'
 import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
-import { PrismaService } from '../prisma/prisma.service'
+import { DatabaseModule } from '../database/database.module'
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [
     CreateAccountController,
     AuthenticateController,
     CreateQuestionsController,
     FetchRecentQuestionsController,
   ],
-  providers: [PrismaService],
 })
 export class HttpModule {}
